4ad8f0a761f049f520ec2d18c4f467ecb86840ba
André Primo de Oliveira <andreprimooliveira@it6ti.com.br>
2026-07-21 16:07:27 -0300

    primeiro commit

---
+++ tests/SomaTest.php
@@ -0,0 +1,9 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class SomaTest extends TestCase {
+  public function testSomaDoisValores() {
+    $resultado = 2 + 3;
+    $this->assertEquals(5, $resultado);
+  }
+}
